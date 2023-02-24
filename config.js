61f23109c1cfa17dabe275f5583fa0c61d376c15
tarun <124577178+1997break@users.noreply.github.com>
2023-02-24 17:37:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const fs = require('fs')
 const chalk = require('chalk')
 //git test by Sam    huh
-global.autoTyping = false //auto tying in gc (true to on, false to off)
-global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = true //auto reading in gc (true to on, false to off)
-global.autoReadAll = false // auto reading in all pchat gc and status (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoReadAll = true // auto reading in all pchat gc and status (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 global.thum = fs.readFileSync("./ZeroTwoMedia/image3.jpg") //ur thumb pic
 global.log0 = fs.readFileSync("./ZeroTwoMedia/image3.jpg") //ur logo pic
